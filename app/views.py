f9cb8d07d118fbe88a45ca6fdd92711835e546f6
Billy Cao <billy.cao@ucla.edu>
2012-07-29 23:53:20 -0700

    Code generation and UI tweaks

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ from models import *
 class MainPage(webapp.RequestHandler):
   def get(self):
     game_name = self.request.get('game_name') or 'MTV'
-    user_name = users.get_current_user().nickname()
+    player_name = users.get_current_user().nickname()
 
     games = Game.all()
     if not games.count():
@@ -22,7 +22,7 @@ class MainPage(webapp.RequestHandler):
         return
     # TODO(billycao): Encapsulate database query calls into some sort of interface
     my_missions = Mission.in_game(game_name).filter(
-        'assassin =', user_name)
+        'assassin =', player_name)
 
     # Game statistics
     num_players = Player.in_game(game_name).count()
@@ -31,8 +31,13 @@ class MainPage(webapp.RequestHandler):
 
     # Player stats
     is_registered = Player.in_game(game_name).filter(
-        'nickname =', user_name).count()
-    target_mission = my_missions.filter('timestamp =', None).get()
+        'nickname =', player_name).count()
+    player_code = ''
+    target_mission = None
+    if is_registered:
+        player_code = Player.in_game(game_name).filter(
+            'nickname =', player_name).get().code
+        target_mission = my_missions.filter('timestamp =', None).get()
 
     if users.get_current_user():
       url = users.create_logout_url(self.request.uri)
@@ -42,7 +47,7 @@ class MainPage(webapp.RequestHandler):
       url_linktext = 'Login'
 
     template_values = {
-      'curr_game': game_name,
+      'game_name': game_name,
       'game_started': Game.has_started(game_name),
       'games': games,
       'is_registered': is_registered,
@@ -50,7 +55,8 @@ class MainPage(webapp.RequestHandler):
       'target_mission': target_mission,
       'url': url,
       'url_linktext': url_linktext,
-      'user_name': user_name,
+      'player_name': player_name,
+      'player_code': player_code,
       'winner': winner,
       'FLAGS_show_game_title': os.environ['show_game_title'] == "True"
     }
@@ -69,6 +75,7 @@ class JoinGame(webapp.RequestHandler):
       if not exists:
         player = Player(parent=game_key(game_name),
             key_name=users.get_current_user().nickname())
+        player.code = Player.newcode()
         player.nickname = users.get_current_user().nickname()
         player.uid = users.get_current_user().user_id()
         player.email = users.get_current_user().email()
