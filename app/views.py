579bb97e23b141bca98cbdace716e6553e752625
Nicholas Solichin <nrsolichin@gmail.com>
2012-08-01 23:35:33 -0700

    CSRF prevention

--- app/views.py
+++ app/views.py
@@ -2,12 +2,14 @@
 
 import sys, os
 import urllib
+import json
 
 from google.appengine.api import urlfetch
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import webapp
-from models import *
+from app.models import *
+from lib import csrf
 
 class MainPage(webapp.RequestHandler):
   def get(self):
@@ -68,6 +70,7 @@ class MainPage(webapp.RequestHandler):
       url_linktext = 'Login'
 
     template_values = {
+      'csrf_token': csrf.get_csrf_token(player_name),
       'game_name': game_name,
       'game_started': game_started,
       'games': games,
@@ -112,7 +115,7 @@ class JoinGame(webapp.RequestHandler):
     else:
       url = users.create_login_url(self.request.uri)
       self.response.out.write("<a href=\"%s\">Login</a>" % url)
-        
+
 class Kill(webapp.RequestHandler):
   def post(self):
     game_name = self.request.get('game_name')
@@ -120,22 +123,44 @@ class Kill(webapp.RequestHandler):
     ksuccesstext = "true"
 
     if users.get_current_user():
-      player = Player.get(game_name, users.get_current_user().nickname())
+      player_name = users.get_current_user().nickname()
+      if not csrf.check_csrf_token(player_name, self.request.get('csrf_token')):
+        self.response.out.write(json.dumps({
+          'status': 'error',
+          'message': "Invalid token. Please refresh the page and try again."
+        }))
+        return
+      player = Player.get(game_name, player_name)
       mission = Mission.in_game(game_name).filter(
-        'assassin =', users.get_current_user().nickname()).filter('timestamp =', None).get()
+        'assassin =', player_name).filter('timestamp =', None).get()
       victim = Player.get(game_name, mission.victim)
       
       if player.code.lower() == code:
         try:
-          player.die(users.get_current_user().nickname())
-          self.response.out.write(ksuccesstext)
+          player.die(player_name)
+          self.response.out.write(json.dumps({
+            'status': 'success',
+            'message': "You've commited suicide!"
+          }))
         except AssassinationException, e:
-          self.response.out.write(e)
+          self.response.out.write(json.dumps({
+            'status': 'error',
+            'message': "Cannot commit suicide. (%s)" % e
+          }))
       elif victim.code.lower() == code:
         try:
-          victim.die(users.get_current_user().nickname())
-          self.response.out.write("Congratulations! You've killed %s!" % victim.nickname)
+          victim.die(player_name)
+          self.response.out.write(json.dumps({
+            'status': 'success',
+            'message': "Congratulations! You've killed %s!" % victim.nickname
+          }))
         except AssassinationException, e:
-          self.response.out.write(e)
+          self.response.out.write(json.dumps({
+            'status': 'error',
+            'message': "Cannot kill target. (%s)" % e
+          }))
       else:
-        self.response.out.write("Invalid code.")
+        self.response.out.write(json.dumps({
+          'status': 'error',
+          'message': "Invalid code."
+        }))
